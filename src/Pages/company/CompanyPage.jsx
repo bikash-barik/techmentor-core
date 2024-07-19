b930440dd77d0838a068a28c998c9b6aa798ced8
sarthak singh <buddysuccess7@gmail.com>
2024-07-19 15:04:29 +0530

    done

--- src/Pages/company/CompanyPage.jsx
+++ src/Pages/company/CompanyPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy } from "react";
 import "./company.css";
 import Img1 from "./../../Assets/img1.jpg";
 import Img2 from "./../../Assets/img2.jpg";
@@ -12,8 +12,9 @@ const CompanyPage = () => {
         <div className="">
           <div className=" other">
             <img
-            data-aos="flip-right"
               src={Img1}
+              data-aos="flip-left"
+              loading={lazy}
               className="img1"
               alt="IntroductionImage"
               style={{ border: "1px solid #ddd" }}
@@ -77,71 +78,101 @@ const CompanyPage = () => {
       <div class="goal text-center">
         <h1 className="mb-4">Goal</h1>
         <div class="goall">
-          <div class="wid">
-            <h2>Marketing and Promotions</h2>
-            <ul class="ull">
-              <li>
-                Attending Education Fairs i.e. StudyWorld, Alphe, UK NARIC
-              </li>
-              <li>Participating in educational and agents' events</li>
-              <li>
-                Attending International conference i.e. Going Global Conference
-                (British Council), Gulf Education Conference
-              </li>
-              <li>
-                Maintaining liaison with Education Providers i.e. local and
-                international feeder institutions
-              </li>
-              <li>Placing Advertisement in popular Newspapers</li>
-              <li>Arranging leaflets, flyers distribution at focal points</li>
-            </ul>
+          <div className="datas">
+            <img
+              src={Img2}
+              data-aos="flip-left"
+              loading={lazy}
+              className="img1"
+              alt="IntroductionImage"
+              style={{ border: "1px solid #ddd" }}
+            />
+            <div class="wid">
+              <h2>Marketing and Promotions</h2>
+              <ul class="ull">
+                <li>
+                  Attending Education Fairs i.e. StudyWorld, Alphe, UK NARIC
+                </li>
+                <li>Participating in educational and agents' events</li>
+                <li>
+                  Attending International conference i.e. Going Global
+                  Conference (British Council), Gulf Education Conference
+                </li>
+                <li>
+                  Maintaining liaison with Education Providers i.e. local and
+                  international feeder institutions
+                </li>
+                <li>Placing Advertisement in popular Newspapers</li>
+                <li>Arranging leaflets, flyers distribution at focal points</li>
+              </ul>
+            </div>
           </div>
-          <div class="wid">
-            <h2>Professional training undertaken</h2>
-            <ul class=" ull">
-              <li>
-                ABritish Council - Education UK Certificate for Counsellor
-              </li>
-              <li>English UK Partner Agency Training</li>
-              <li>
-                ICEF Agent Training Course to support excellence and
-                professional standards in international education
-              </li>
-              <li>
-                PIER (Professional International Education Resources) Agent
-                Trainings
-              </li>
-              <li>
-                ENZ - agent training about studying and living in New Zealand
-              </li>
-              <li>
-                QEAC Certificate (Pier) - Training for studying and living in
-                Australia
-              </li>
-            </ul>
+          <div className="datass">
+            <img
+              src={Img3}
+              data-aos="flip-right"
+              loading={lazy}
+              className="img1"
+              alt="IntroductionImage"
+              style={{ border: "1px solid #ddd" }}
+            />
+            <div class="wid">
+              <h2>Professional training undertaken</h2>
+              <ul class=" ull">
+                <li>
+                  ABritish Council - Education UK Certificate for Counsellor
+                </li>
+                <li>English UK Partner Agency Training</li>
+                <li>
+                  ICEF Agent Training Course to support excellence and
+                  professional standards in international education
+                </li>
+                <li>
+                  PIER (Professional International Education Resources) Agent
+                  Trainings
+                </li>
+                <li>
+                  ENZ - agent training about studying and living in New Zealand
+                </li>
+                <li>
+                  QEAC Certificate (Pier) - Training for studying and living in
+                  Australia
+                </li>
+              </ul>
+            </div>
           </div>
-          <div class="wid">
-            <h2>Growth strategy</h2>
-            <ul class=" ull">
-              <li>
-                Our short-term plan is to increase the number of our partner
-                educational
-              </li>
-              <li>
-                {" "}
-                institutions so that international students can enjoy more
-                flexibility in their choices
-              </li>
-              <li>
-                and we have courses and educational institutions for every
-                single and specialised
-              </li>
-              <li>
-                choice. Our long-term strategy is to open offices in Middle
-                East, Far East, East &
-              </li>
-              <li>South Asia, North America, Africa and Australia.</li>
-            </ul>
+          <div className="datas">
+            <img
+              src={Img4}
+              data-aos="flip-right"
+              loading={lazy}
+              className="img1"
+              alt="IntroductionImage"
+              style={{ border: "1px solid #ddd" }}
+            />
+            <div class="wid">
+              <h2>Growth strategy</h2>
+              <ul class=" ull">
+                <li>
+                  Our short-term plan is to increase the number of our partner
+                  educational
+                </li>
+                <li>
+                  {" "}
+                  institutions so that international students can enjoy more
+                  flexibility in their choices
+                </li>
+                <li>
+                  and we have courses and educational institutions for every
+                  single and specialised
+                </li>
+                <li>
+                  choice. Our long-term strategy is to open offices in Middle
+                  East, Far East, East &
+                </li>
+                <li>South Asia, North America, Africa and Australia.</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
@@ -149,8 +180,7 @@ const CompanyPage = () => {
         <h2 class="text-center my-4">Our Team</h2>
         <div class="row">
           <div class="col-md-4 mb-4">
-            <div  data-aos="fade-right" class="card">
-              <img src={Img2} alt="Jane" class="card-img-top" />
+            <div data-aos="fade-right" class="card">
               <div class="card-body">
                 <h4 class="card-title">Jane Doe</h4>
                 <p class="card-text text-muted">CEO & Founder</p>
@@ -165,8 +195,7 @@ const CompanyPage = () => {
             </div>
           </div>
           <div class="col-md-4 mb-4">
-            <div  data-aos="fade-left" class="card">
-              <img src={Img3} alt="Mike" class="card-img-top" />
+            <div data-aos="fade-left" class="card">
               <div class="card-body">
                 <h4 class="card-title">Mike Ross</h4>
                 <p class="card-text text-muted">Art Director</p>
@@ -181,8 +210,7 @@ const CompanyPage = () => {
             </div>
           </div>
           <div class="col-md-4 mb-4">
-            <div  data-aos="fade-right" class="card">
-              <img src={Img4} alt="John" class="card-img-top" />
+            <div data-aos="fade-right" class="card">
               <div class="card-body">
                 <h4 class="card-title">John Doe</h4>
                 <p class="card-text text-muted">Designer</p>
